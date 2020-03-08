c180bd7ab34af3377e7a7a6e77a247b6569ee3a0
John Hastings <JohnHastingsx95@gmail.com>
2020-03-08 14:45:39 -0400

    msg

---
+++ HS_ThumbFiddle.py
@@ -0,0 +1,26 @@
+import os
+
+def prompt(prefix):
+    command_line = "$"
+    print(command_line)
+    usr = input(prefix + " ")
+    return usr
+
+
+user = prompt("username")
+print("your username is " + user)
+cwd = os.getcwd()
+if not os.path.isdir(cwd+'/'+user):
+    os.mkdir(user)
+    os.chdir(user+'/')
+    print(user + ' dosent exsist. creating dir...')
+    conf = open(user+'.conf', 'w')
+    conf.write(user)
+    conf.close
+else:
+    print(user + 'already exsists')
+prompt = ""
+while prompt != '-q':
+    prompt = prompt(user+'>')
+print('program end')
+
