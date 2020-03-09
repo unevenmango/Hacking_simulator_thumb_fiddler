fa351079c7b0e67f21e6ebc6e48498f9b70caafb
John Hastings <JohnHastingsx95@gmail.com>
2020-03-08 22:56:57 -0400

    Tranfering functions over
    also allowd to test them using the paramiters

--- HS_ThumbFiddle.py
+++ HS_ThumbFiddle.py
@@ -14,54 +14,64 @@ def NameGen():
     else:        
         servername = random.choice(sname)    
     return servername
-
-
-
-
-
-
-def ipgen(gate):        
-   if gate == 'gate':
-       ip1 = random.randint(10, 99) 
-       ip2 = random.randint(10, 99) 
-       ip3 = random.randint(100, 999)  
-       ipgen = str(ip1) + '.' + str(ip2) + '.' + str(ip3) 
-   elif gate == 'ran':   
-       ip1 = random.randint(100, 400)
-       ip2 = random.randint(150, 150)
-       ip3 = random.randint(100, 999)
-       ip4 = random.randint(10, 150)
-       ipgen = str(ip1) + '.' + str(ip2) + '.' + str(ip3) + '.' + str(ip4)
+def ip_gen(option='null'):        
+    if option == 'local':
+        ip1 = random.randint(10, 99) 
+        ip2 = random.randint(10, 99) 
+        ip3 = random.randint(100, 999)  
+        ip_gen = str(ip1) + '.' + str(ip2) + '.' + str(ip3) 
+    elif option == 'ran':   
+        ip1 = random.randint(100, 400)
+        ip2 = random.randint(150, 150)
+        ip3 = random.randint(100, 999)
+        ip4 = random.randint(10, 150)
+        ip_gen = str(ip1) + '.' + str(ip2) + '.' + str(ip3) + '.' + str(ip4)
     else:
-       ip1 = random.randint(100, 999)        
-       ip2 = random.randint(10, 99)        
-       ip3 = random.randint(100, 999)
-       ipgen = str(ip1) + '.' + str(ip2) + '.' + str(ip3)
-   return ipgen
+        ip1 = random.randint(100, 999)        
+        ip2 = random.randint(10, 99)        
+        ip3 = random.randint(100, 999)
+        ip_gen = str(ip1) + '.' + str(ip2) + '.' + str(ip3)
+    return ip_gen
+def mac_gen():
+    return "indev"
 
+def ip_setup():
+    port_types = {22:"ssh",21:"ftp",8080:"https"}
+    ip_conf = {}
+    for port in port_types:
+        ip_conf[port] = {"local_ip":ip_gen('local'),"service":port_types[port]}
+    return ip_conf
+def Generator(ip_count):
+    dyn_num = 0
+    Ip_dict = {}
+    while dyn_num < ip_count:
+        dyn_num = dyn_num + 1
+        Ip_dict[ip_gen()] = ip_setup()
+    return Ip_dict
 
 def prompt(prefix):
     command_line = "$"
     print(command_line)
     usr = input(prefix + " ")
     return usr
-if sys.argv[1] == "start":
-    user = prompt("username")
-    print("your username is " + user)
-    cwd = os.getcwd()
-    if not os.path.isdir(cwd+'/'+user):
-        os.mkdir(user)
-        os.chdir(user+'/')
-        print(user + ' dosent exsist. creating dir...')
-        conf = open(user+'.conf', 'w')
-        conf.write(user)
-        conf.close
+if (len(sys.argv) > 1):
+    if sys.argv[1] == "start":
+        user = prompt("username")
+        print("your username is " + user)
+        cwd = os.getcwd()
+        if not os.path.isdir(cwd+'/'+user):
+            os.mkdir(user)
+            os.chdir(user+'/')
+            print(user + ' dosent exsist. creating dir...')
+            conf = open(user+'.conf', 'w')
+            conf.write(user)
+            conf.close
+        else:
+            print(user + 'already exsists')
+        prompt = ""
+        while prompt != '-q':
+            prompt = prompt(user+'>')
     else:
-        print(user + 'already exsists')
-    prompt = ""
-    while prompt != '-q':
-        prompt = prompt(user+'>')
-else:
-    print(eval(sys.argv[1]))
+        print(eval(sys.argv[1])) 
 print('program end')
 
