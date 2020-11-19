91d813b28e606d658cbaf9d674c503048c03c8ac
unevenmango <johnhastingsx95@gmail.com>
2020-11-18 20:57:42 -0500

    ops

--- testterm.py
+++ testterm.py
@@ -1,39 +1,47 @@
 
 
-
-
 datafile = "test.dat"
 c = open(datafile, "w")
 c.close
-f = open(datafile, "r")
-if not f:
-    exit("data file failed to init")
 
-notes = {}
-data = f.read().split("\n")
-f.close
-for l in data:
-    if "#" in l:
-        notes[l.split("#")[0]] = l.split("#")[1]
 def main():
     opt = input(">> ")
-    print("you typed " + opt)
     
     if opt == "q":
-        f.close()
+        
         exit("exited")
     elif opt == "n":
-        title = input("Title : ")
-        note = input("Note : ")
-        if title in notes.keys():
-            exit("note already exists")
-        else:
-            dta = "\n".join(data)
-            f = open(datafile, "w")
-            f.write(dta + "\n" + title + "#" + note)
-            print(title + " added")
-            f.close()
-
+        Notes()
     main()
 
+
+
+
+def Read(path):
+    f = open("path")
+
+
+
+def Notes():
+    title = input("Title : ")
+        
+    f = open(datafile, "r")
+
+    if not f:
+        exit("data file failed to init")
+    notes = {}
+    data = f.read().split("\n")
+    f.close
+    for l in data:
+        if "#" in l:
+            notes[l.split("#")[0]] = l.split("#")[1]
+    if title in notes.keys():
+        exit("note already exists")
+    
+    note = input("Note : ")
+    dta = "\n".join(data)
+    f = open(datafile, "w")
+    f.write(dta + "\n" + title + "#" + note)
+    print(title + " added")
+    f.close()
 main()
